91162c2ea249f4a7339eedefacaae4717a92c4c9
Alejandrotrianam644549 <alejandro.trianam27@gmail.com>
2024-10-31 09:02:13 -0500

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,1 +1,23 @@
+abstract class Animal {
+    private String nombre;
+    private int edad;
+
+    // Constructor de Animal
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Metodo abstracto hacer sonido
+    public abstract void hacerSonido();
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
 
