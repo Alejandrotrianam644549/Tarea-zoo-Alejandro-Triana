3323e4f6bf16db011d13d8397666a7edb66eb52b
Alejandrotrianam644549 <alejandro.trianam27@gmail.com>
2024-10-31 09:02:52 -0500

    Update Gato.java

--- src/Gato.java
+++ src/Gato.java
@@ -1,1 +1,10 @@
+class Gato extends Animal {
+    public Gato(String nombre, int edad) {
+        super(nombre, edad);
+    }
 
+    @Override
+    public void hacerSonido() {
+        System.out.println("El gato maulla");
+    }
+}
