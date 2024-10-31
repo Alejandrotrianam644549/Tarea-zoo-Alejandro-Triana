3cd52762b7f3782fc63f1763f313b725fc5f285c
Alejandrotrianam644549 <alejandro.trianam27@gmail.com>
2024-10-31 09:03:17 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear el zoológico
+        Zoologico zoologico = new Zoologico();
 
+        // Crear instancias de 2 Perro y 2 Gato
+        Animal gato1 = new Gato("Simón", 3);
+        Animal gato2 = new Gato("Ron", 2);
+        Animal perro1 = new Perro("Paco", 5);
+        Animal perro2 = new Perro("Rex", 4);
+
+        // Agregar animales al zoológico
+        zoologico.agregarAnimal(gato1);
+        zoologico.agregarAnimal(gato2);
+        zoologico.agregarAnimal(perro1);
+        zoologico.agregarAnimal(perro2);
+
+        // Mostrar sonidos de todos los animales
+        zoologico.mostrarSonidos();
+
+        // Mostrar el conteo de animales por tipo
+        zoologico.mostrarConteoPorTipo();
+    }
+}
